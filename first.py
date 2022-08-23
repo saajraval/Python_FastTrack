99bffa49e3ef75aef484881f3621fdb9e3742e6b
saajraval <saajravals7r@gmail.com>
2022-08-23 09:44:23 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,5 @@
+#print("Yesterday was printed as ",' "75 th Independence Day" ')
+
+#print(' On this Occasion, the Prime Minister said to the nation "We are proud to say we',"didn't",'do anything wrong as a nation in the past 75 years ." ')
+
+print('The locatoion of Python Program is : "C:\\newfolder\\temp\\python\\"')
